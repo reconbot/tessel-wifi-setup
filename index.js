61cd21820fc27937f2536d08506f89e4c8ab0682
Nick Hehr <headhipster@hipsterbrown.com>
2016-06-17 16:24:15 -0400

    fix(index): fixes standard style errors

--- index.js
+++ index.js
@@ -1,61 +1,69 @@
 // core modules
-var fs = require('fs');
-var http = require('http');
-var path = require('path');
-var querystring = require('querystring');
+var fs = require('fs')
+var http = require('http')
+var path = require('path')
 
 // server route setup
 var server = http.createServer((request, response) => {
   switch (request.url) {
     case '/':
       fs.readFile(path.join(__dirname, 'index.html'), (error, content) => {
-        response.writeHead(200, 'OK', { 'Content-Type': 'text/html' });
-        response.end(content);
-      });
-      break;
+        if (error) {
+          throw error
+        }
+
+        response.writeHead(200, 'OK', { 'Content-Type': 'text/html' })
+        response.end(content)
+      })
+      break
 
     case '/formhandler':
       if (request.method === 'POST') {
-        var requestData = '';
+        var requestData = ''
 
-        request.on('data', (chunk) => requestData += chunk.toString());
+        request.on('data', (chunk) => { requestData += chunk.toString() })
 
         request.on('end', () => {
           fs.readFile(path.join(__dirname, 'form-success.html'), (error, content) => {
-            response.writeHead(200, 'OK', { 'Content-Type': 'text/html' });
-            response.end(content);
-          });
-        });
+            if (error) {
+              throw error
+            }
+
+            response.writeHead(200, 'OK', { 'Content-Type': 'text/html' })
+            response.end(content)
+          })
+        })
       } else {
-        handle404(response);
+        handle404(response)
       }
-      break;
+      break
 
     case '/stop-setup':
-      response.writeHead(200, 'OK', { 'Content-Type': 'text/html' });
-      response.end('Wifi setup complete. Server has stopped.');
-      server.close(() => console.log('Wifi setup complete. Server has stopped'));
-      break;
+      response.writeHead(200, 'OK', { 'Content-Type': 'text/html' })
+      response.end('Wifi setup complete. Server has stopped.')
+      server.close(() => console.log('Wifi setup complete. Server has stopped'))
+      break
+
     default:
-      handle404(response);
-      break;
+      handle404(response)
+      break
   }
-});
+})
 
-server.listen(process.argv[2] || 8080);
+server.listen(process.argv[2] || 8080)
 
-console.log('Server running at %d', process.argv[2] || 8080);
+console.log('Server running at %d', process.argv[2] || 8080)
 
 // response functions
 
 function handle404 (response) {
   fs.readFile(path.join(__dirname, '404.html'), (error, content) => {
     if (error) {
-      console.log(error);
-      content = error.toString();
+      console.log(error)
+      content = error.toString()
     }
 
-    response.writeHead(404, 'Not found', { 'Content-Type': 'text/html' });
-    response.end(content);
-  });
+    response.writeHead(404, 'Not found', { 'Content-Type': 'text/html' })
+    response.end(content)
+  })
 }
